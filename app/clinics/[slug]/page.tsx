3c45073f613de7bc671f53a18b4a4c5725cb9eb4
Muhraf2020 <143078152+Muhraf2020@users.noreply.github.com>
2025-11-05 19:48:49 +0400

    Update page.tsx

--- app/clinics/[slug]/page.tsx
+++ app/clinics/[slug]/page.tsx
@@ -1,4 +1,5 @@
-export const dynamic = 'force-dynamic';
+//export const dynamic = 'force-dynamic';
+export const revalidate = 3600; // 1 hour
 
 import { createSupabaseClient } from '@/lib/supabase';
 import { Clinic } from '@/lib/dataTypes';
